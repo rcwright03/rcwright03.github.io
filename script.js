dc90c9129e979f8720ed31bfd200840fcd744a34
Ryan Wright <rcwright@wpi.edu>
2026-05-27 09:57:03 -0400

    added script.js file for opening tabs in projects section

---
+++ script.js
@@ -0,0 +1,12 @@
+function openTab(evt, tab_name) {
+    var i, tab_content, tab_buttons;
+
+    // hide content in other tabs
+    tab_content = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tab_content.length; i++) {
+        tab_content[i].style.display = "none";
+    }
+
+    // display element that was clicked on
+    document.getElementById(tab_name).style.display = "block";
+}
