515221e2d9b838caa97455e22ee00d13e1e69607
Ryan Wright <rcwright@wpi.edu>
2026-05-29 13:15:10 -0400

    centered tab content, added highlighting for tab buttons when hovered and clicked on, changed styling from javascript to CSS

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-function openTab(evt, tab_name) {
-    var i, tab_content, tab_buttons;
+function openTab(evt, tab_name, tab_button_name) {
+    var i, tab_content, tab_button;
 
     // hide content in other tabs
     tab_content = document.getElementsByClassName("tabcontent");
+    tab_button = document.getElementsByClassName("tablinks");
     for (i = 0; i < tab_content.length; i++) {
         tab_content[i].style.display = "none";
+        tab_button[i].classList.remove("active");
     }
 
-    // display element that was clicked on
+    // show selected tab content
     document.getElementById(tab_name).style.display = "block";
 
-    console.log("test")
+    // highlight selected tab button
+    document.getElementById(tab_button_name).classList.add("active");
 }
 
 window.onload = function () {
-    document.getElementById("default_tab").click()
+    document.getElementById("ds_tab_button").click();
+    document.getElementById("ds_tab_button").classList.add("active");
 }
