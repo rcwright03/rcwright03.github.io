66588b17109da8b7794a938babd22e3a3ed71f0e
Ryan Wright <rcwright@wpi.edu>
2026-05-31 21:32:16 -0400

    starting to reformat projects section into a grid and made other changes

--- script.js
+++ script.js
@@ -17,6 +17,71 @@ function openTab(evt, tab_name, tab_button_name) {
 }
 
 window.onload = function () {
+    updateProject("ds");
+    updateProject("cs");
+    updateProject("ss");
     document.getElementById("ds_tab_button").click();
-    document.getElementById("ds_tab_button").classList.add("active");
+    //document.getElementById("ds_tab_button").classList.add("active");
+}
+
+const projects = {
+    ds: [
+        {
+            image: "../thumbnails/songbird.png",
+            title: "Songbird",
+            description: "AI Song Generator (DS TEST)"
+        },
+        {
+            image: "../../ryan_square.jpg",
+            title: "me!",
+            description: "me but square!"
+        }
+    ],
+    cs: [
+        {
+            image: "../thumbnails/songbird.png",
+            title: "Songbird",
+            description: "AI Song Generator (CS TEST)"
+        },
+        {
+            image: "../../ryan_square.jpg",
+            title: "me!",
+            description: "me but square!"
+        }
+    ],
+    ss: [
+        {
+            image: "../thumbnails/songbird.png",
+            title: "Songbird",
+            description: "AI Song Generator (CS TEST)"
+        },
+        {
+            image: "../../ryan_square.jpg",
+            title: "me!",
+            description: "me but square!"
+        }
+    ]
+}
+
+const currentIndex = {
+    ds: 0,
+    cs: 0,
+    ss: 0
+}
+
+function updateProject(tab) {
+    const project = projects[tab][currentIndex[tab]];
+    document.getElementById(`${tab}_image`).src = project.image;
+    document.getElementById(`${tab}_title`).textContent = project.title;
+    document.getElementById(`${tab}_description`).textContent = project.description;
+}
+
+function changeProject(tab, direction) {
+    const numProjects = projects[tab].length;
+
+    currentIndex[tab] =
+        (currentIndex[tab] + direction + numProjects) %
+        numProjects;
+
+    updateProject(tab);
 }
